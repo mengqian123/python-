fb91baf47d73d760898f1d94cf943ea68b61f574
xq <767697485@qq.com>
2018-09-15 09:53:34 +0800

    第三次提交

---
+++ project - 副本 (3)/app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse('第一个视图')
+
+
+def detail(request,question_id):
+    return HttpResponse('you are looking at question %s' %question_id)
+
+def results(request,question_id):
+    response='you are looking at results %s'
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
